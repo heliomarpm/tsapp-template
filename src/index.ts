3b782a2ab6efa63d527fb772ef16a8ef7a9f3051
Heliomar P. Marques <heliomarpm@gmail.com>
2025-05-10 19:00:04 -0300

    feat: initialize

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Template for Typescript applications
+ * @module helloWorld
+ * @author Heliomar P. Marques
+ */
+
+/**
+ * Returns the string "Hello, World!".
+ * @returns "Hello, World!"
+ * @example
+ * helloWorld(); // returns "Hello, World!"
+ */
+function helloWorld() {
+	return "Hello, World!";
+}
+
+/**
+ * A simple function that returns "Hello, World!".
+ * @default
+ */
+export { helloWorld };
